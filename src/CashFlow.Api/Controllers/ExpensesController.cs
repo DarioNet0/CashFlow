226f05e83c8c03583e5c4f8e045933683fc7c0bb
DarioNet0 <dariooliveirasneto@gmail.com>
2024-06-19 12:27:19 -0300

    Add project files.

---
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,19 @@
+﻿using CashFlow.Application.UseCases.Exepenses.Register;
+using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestExpenseJson request)
+        {
+            var UseCase = new RegisterExpenseUseCase(); 
+            var response = UseCase.Execute(request);
+            return Created(string.Empty, response);
+        }
+    }
+}
---
+++ src/CashFlow.Application/UseCases/Exepenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,13 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Exepenses.Register
+{
+    public class RegisterExpenseUseCase
+    {
+        public ResponseRegisterExpenseJson Execute(RequestExpenseJson request)
+        {
+            return new ResponseRegisterExpenseJson();
+        }
+    }
+}
