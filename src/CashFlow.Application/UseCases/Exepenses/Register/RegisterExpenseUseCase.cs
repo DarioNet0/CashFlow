45ebe52ae9cf996f88189b2ee9cc4c1e15ce76ba
DarioZemaTI <dario.neto@zema.com>
2024-06-24 10:39:08 -0300

    exception

--- src/CashFlow.Application/UseCases/Exepenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Exepenses/Register/RegisterExpenseUseCase.cs
@@ -1,7 +1,5 @@
-﻿using CashFlow.Communication.Enums;
-using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
-using System.ComponentModel.DataAnnotations;
 
 namespace CashFlow.Application.UseCases.Exepenses.Register {
     public class RegisterExpenseUseCase
@@ -12,21 +10,13 @@ namespace CashFlow.Application.UseCases.Exepenses.Register {
             return new ResponseRegisterExpenseJson();
         }
         private void Validate(RequestExpenseJson request) {
-            var titleEmpty = string.IsNullOrWhiteSpace(request.Title);
-            if (titleEmpty) {
-                throw new ArgumentException("The Title is required");
+            var validator = new RegisterExpenseValidator();
+            var result = validator.Validate(request);
+            if (result.IsValid == false) {
+                var errorMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
+                throw new ArgumentException(errorMessages.ToString());
             }
-            if (request.Amount <= 0) {
-                throw new ArgumentException("The Amount must be greater than 0");
-            }
-            var result = DateTime.Compare(request.Date, DateTime.UtcNow);
-            if (result  > 0) {
-                throw new ArgumentException("Expenses cannot be for the furute");
-            }
-            var paymentTypeIsValid =  Enum.IsDefined(typeof(PaymentType), request.paymentType);
-            if (paymentTypeIsValid == false) {
-                throw new ArgumentException("Payment Type Is Not Valid");
-            }
+            
         }
     }
 }
---
+++ src/CashFlow.Application/UseCases/Exepenses/Register/RegisterExpenseValidator.cs
@@ -0,0 +1,13 @@
+﻿using CashFlow.Communication.Requests;
+using FluentValidation;
+
+namespace CashFlow.Application.UseCases.Exepenses.Register;
+public class RegisterExpenseValidator : AbstractValidator<RequestExpenseJson> {
+    public RegisterExpenseValidator()
+    {
+        RuleFor(expense => expense.Title).NotEmpty().WithMessage("The Title is required");
+        RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage("The Amount must be greater than 0");
+        RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.Now).WithMessage("Expenses cannot be for the furute");
+        RuleFor(expense => expense.paymentType).IsInEnum().WithMessage("Payment Type Is Not Valid");
+    }
+}
