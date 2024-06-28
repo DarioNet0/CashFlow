0107b507b2acc098bdd57b9030db115ae0241ad8
DarioZemaTI <dario.neto@zema.com>
2024-06-28 12:24:12 -0300

    Testes de Unidade

---
+++ src/CashFlow.Api/Middleware/CultureMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace CashFlow.Api.Middleware;
+
+public class CultureMiddleware {
+    private readonly RequestDelegate _next;
+    public CultureMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+    public async Task Invoke(HttpContext context) {
+        var supportedLanguage = CultureInfo.GetCultures(CultureTypes.AllCultures).ToList();//criando uma lista com todas as tags possíveis
+        var requestedCulture = context.Request.Headers.AcceptLanguage.FirstOrDefault(); //extraindo da requisição, o idioma
+        var cultureInfo = new CultureInfo("en"); // Setando idioma padrão caso não tenha idioma no header
+        if (string.IsNullOrWhiteSpace(requestedCulture) == false 
+            && supportedLanguage.Exists(l => l.Equals(requestedCulture))) {
+            cultureInfo = new CultureInfo(requestedCulture); // reescrevendo o valor padrão antes setado
+        }
+        CultureInfo.CurrentCulture = cultureInfo;
+        CultureInfo.CurrentUICulture = cultureInfo;
+        await _next(context); //permitindo seguir para o endpoint
+    }
+}
