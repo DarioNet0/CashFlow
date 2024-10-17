3804f54b5a4bc21d20b331c5e9bc63ea8d490fd6
Dário Oliveira <dariooliveirasneto@gmail.com>
2024-10-17 20:29:14 -0300

    release fix

--- tests/Validator.Tests/Expenses/User/Register/RegisterUserValidatorTest.cs
+++ tests/Validator.Tests/Expenses/User/Register/RegisterUserValidatorTest.cs
@@ -1,19 +1,11 @@
-﻿using CashFlow.Application.UseCases.User;
-using CommonTestUtilities.Requests;
-
-namespace Validator.Tests.Expenses.User.Register
+﻿namespace Validator.Tests.Expenses.User.Register
 {
     public class RegisterUserValidatorTest
     {
         [Fact]
         public void Succes()
         {
-            //Arrange
-            var validator = new UserValidator();
-            var request = RequestRegisterExpenseJsonBuild.Build();
 
-            //Act
-            validator.Validate
         }
     }
 }
